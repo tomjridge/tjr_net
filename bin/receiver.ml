5929c264ee98c54319c9ad39f462fdaf8f5271d7
Tom Ridge <tom.j.ridge@googlemail.com>
2017-10-28 12:01:09 +0100

    +

--- bin/receiver.ml
+++ bin/receiver.ml
@@ -1,12 +1,14 @@
 open Tjr_connection
 open Unix_
 
+let log_ x = ()
+
 let main () = 
-  print_endline __LOC__;
+  log_ __LOC__;
   listen_accept ~quad:Shared.recvr >>= function
   | `Listen_accept_incorrect_peername -> failwith __LOC__
   | `Connection conn ->
-    print_endline __LOC__;
+    log_ __LOC__;
     let rec loop i () =
       recv_string ~conn >>= function
       | `Ok msg -> (
@@ -19,4 +21,6 @@ let main () =
     in
     loop 0 ()
 
-let _ = main()
+let _ = main()|> function
+  | Ok x -> ()
+  | Error (e,s1,s2) -> raise (Unix.Unix_error(e,s1,s2))
